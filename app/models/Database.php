10b7d9f92d391d79792bef53888376e6ec614072
manoah kalasa <kalasamanoah@gmail.com>
2025-06-01 22:35:05 +0200

    creation de la classe Database pour la connexion a la base de donnée.

---
+++ app/models/Database.php
@@ -0,0 +1,32 @@
+<?php
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $conn;
+
+    public function __construct() {
+        $this->host = DB_HOST;
+        $this->db_name = DB_NAME;
+        $this->username = DB_USER;
+        $this->password = DB_PASS;
+    }
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("Connection error: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
