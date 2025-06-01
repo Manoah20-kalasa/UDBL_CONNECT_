9755c962a72c8602599c9e6d9136d84cad20335c
manoah kalasa <kalasamanoah@gmail.com>
2025-06-01 23:33:36 +0200

    creation du fichier index.

---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php
+// Configuration de l'application
+define('APP_NAME', 'UDBL CONNECT');
+define('APP_URL', 'http://localhost/udbl-connect');
+
+// Configuration de session
+session_start();
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../config/constants.php';
+require_once '../config/database.php';
+require_once '../app/core/App.php';
+
+$app = new App();
+$app->run();
